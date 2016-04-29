4489f209693b76d4ddee3e8ce29397330dcd4fc2
KeeganAriel <keegan.teinowitz@gmail.com>
2016-04-29 12:57:02 -0700

    trying to get click event to add one whosTurn

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
+	
+	function startGame () {
+		var whosTurn = "X";
+		setMessage(whosTurn + " gets to start!");
+	}
+
+	function setMessage (msg) {
+		$(".message").text(msg);
+	}
+
+
+	$( ".square" ).click(function() {
+		var whosTurn = "0";
+		console.log("you clicked!");
+		$(".square").html(whosTurn);
+
+	});
+
+
+	startGame();
 
 });
+
+	// $( ".square" ).one('click',function(event) {
+	// 	var whosTurn = "0";
+	// 	console.log("you clicked!");
+	// 	$(".square").html(whosTurn);
+
+ //        }
+ //    );
+
+
