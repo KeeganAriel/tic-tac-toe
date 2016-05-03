472a1d32ea536d48d5e12c3c9a34521c21d537e6
KeeganAriel <keegan.teinowitz@gmail.com>
2016-05-03 11:55:09 -0700

    add whosTurn(), whoWon,

--- js/app.js
+++ js/app.js
@@ -1,33 +1,58 @@
+// var whosTurn = "X";
+
 $(document).ready(function(){
-	
+	var whosTurn = "X";
+
 	function startGame () {
-		var whosTurn = "X";
-		setMessage(whosTurn + " gets to start!");
+		setMessage(whosTurn + " gets to first!");
 	}
 
 	function setMessage (msg) {
-		$(".message").text(msg);
+		$(".message").html(msg);
+
 	}
 
-
 	$( ".square" ).click(function() {
-		var whosTurn = "0";
-		console.log("you clicked!");
-		$(".square").html(whosTurn);
-
+		$(this).html(whosTurn);
+		changeTurn();
+		setMessage(whosTurn + " gets to go now...");
+		whoWon();
+		console.log(whosTurn);
 	});
 
+	function changeTurn() {
+		if (whosTurn === "X") {
+			whosTurn = "O";
+		} else {
+			whosTurn = "X";
+		}
+	}
 
+ 	function whoWon() {
+ 		var firstRow = $('.row:first-child .square').text();
+ 		if (firstRow === "XXX") {
+ 			setMessage("X won!");
+ 		} else if (firstRow === "OOO") {
+ 			setMessage("O WON!");
+ 		}
+
+ 		var lastRow = $('.row:last-child .square').text();
+ 		if (lastRow === "XXX") {
+ 			setMessage("X won!");
+ 		} else if (lastRow === "OOO") {
+ 			setMessage("O WON!");
+ 		}
+
+ 		var middleRow = $('.row:nth-child(2) .square').text();
+ 		if (middleRow === "XXX") {
+ 			setMessage("X won!");
+ 		} else if (middleRow === "OOO") {
+ 			setMessage("O WON!");
+ 		}
+ 	}	
 	startGame();
 
 });
 
-	// $( ".square" ).one('click',function(event) {
-	// 	var whosTurn = "0";
-	// 	console.log("you clicked!");
-	// 	$(".square").html(whosTurn);
-
- //        }
- //    );
 
 
